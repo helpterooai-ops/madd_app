97e91fd29276fabc1b176c6fe3edcb07ed0a5c62
YourName <you@example.com>
2026-08-01 02:32:28 +0300

    تحويل build.gradle إلى Kotlin DSL وإصلاح أخطاء الصيغة

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,14 +1,7 @@
-buildscript {
-    ext.kotlin_version = '1.7.10'
-    repositories {
-        google()
-        mavenCentral()
-    }
-
-    dependencies {
-        classpath 'com.android.tools.build:gradle:7.2.0'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
-    }
+plugins {
+    id("com.android.application") version "7.2.0" apply false
+    id("com.android.library") version "7.2.0" apply false
+    kotlin("android") version "1.7.10" apply false
 }
 
 allprojects {
@@ -18,26 +11,17 @@ allprojects {
     }
 }
 
-rootProject.buildDir = '../build'
-subprojects {
-    project.buildDir = "${rootProject.buildDir}/${project.name}"
-}
-subprojects {
-    project.evaluationDependsOn(':app')
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
 
-tasks.register("clean", Delete) {
-    delete rootProject.buildDir
-}
-
-// إجبار جميع المشاريع الفرعية على استخدام JavaVersion.VERSION_1_8
 subprojects {
-    afterEvaluate { project ->
-        if (project.hasProperty('android')) {
-            android {
+    afterEvaluate {
+        if (hasProperty("android")) {
+            extensions.configure<com.android.build.gradle.BaseExtension>("android") {
                 compileOptions {
-                    sourceCompatibility JavaVersion.VERSION_1_8
-                    targetCompatibility JavaVersion.VERSION_1_8
+                    sourceCompatibility = JavaVersion.VERSION_1_8
+                    targetCompatibility = JavaVersion.VERSION_1_8
                 }
             }
         }
